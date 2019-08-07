d3611cbe43e212af5b70b046f6fc182e24cf0c93
santhosh1994m <45985363+santhosh1994m@users.noreply.github.com>
2019-08-07 15:15:26 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+rovider "aws" {
+  shared_credentials_file = "~/.aws/credentials"
+  
+  }
+  
+  resource "aws_vpc" "default" {
+cidr_block = "10.0.0.0/16"
+enable_dns_hostnames = true
+tags = {
+Name = "sanpoc2-aws-vpc"
+}
