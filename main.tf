007543a4e0c23cd0f54ec98e7e6b2ade327ea2d7
santhosh1994m <45985363+santhosh1994m@users.noreply.github.com>
2019-08-07 15:30:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
   
   }
   
-resource "aws_vpc" "default" {
-cidr_block = "10.0.0.0/16"
-enable_dns_hostnames = true
+resource "aws_vpc" "san_test"{
+  cidr_block = "10.0.0.0/16"
 tags = {
-Name = "sanpoc2-aws-vpc"
+    Name = "san_vpc"
+}
 }
